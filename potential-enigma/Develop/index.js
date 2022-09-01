0c136997ec3c703a236ed51374abbe6f847da97a
Edawgg <emccannbusiness@gmail.com>
2022-09-01 08:27:27 -0500

    finalization
    forked my old repo from previous acct. Resubmitting assignment properly

--- potential-enigma/Develop/index.js
+++ potential-enigma/Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./main/generateMarkdown');
+const { writeFile } = require('fs').promises;
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  const questions = () => {
+    return inquirer.prompt([
+      
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use your project?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are the contribution guidelines?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What are the test instructions?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select the license for your project:',
+            choices: ["MIT", "GNU", "Apache", "BSD"],
+        },
+        {
+            type: 'input',
+            name: 'contact',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ]);
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  questions() 
+      // Use writeFile method imported from fs.promises
+    .then((data) => writeFile('README.md', generateMarkdown(data)))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
+};
+
+init();
 
 // Function call to initialize app
 init();
